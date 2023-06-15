69239c5d20340df51d5646a376960df5194bed4e
Dovid Yarmush <yarmushda@gmail.com>
2023-06-15 09:59:27 -0400

    complete anagram algo

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+# create a function that iterates though the list finding
+# words in the list that are similar then sort them the check if any equal 
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+
+        matching_word = sorted(self.word)
+        indices = [index for index, w in enumerate(words) if sorted(w) == matching_word]
+        
+        return[words[index] for index in indices]
